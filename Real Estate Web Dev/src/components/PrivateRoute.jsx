045760e387e3232f5bbae01689de192181429529
HarshanaPrabhath <harshanaprabhath147@gmail.com>
2025-03-10 20:06:06 +0530

    private Route added

--- Real Estate Web Dev/src/components/PrivateRoute.jsx
+++ Real Estate Web Dev/src/components/PrivateRoute.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Navigate,Outlet } from 'react-router'
+import useAuthStatus from '../hooks/useAuthStatus'
 
 function PrivateRoute() {
-    const loggedIn = false
+    const {loggedIn, checkingStatus} = useAuthStatus
    
     return loggedIn ? <Outlet/> : <Navigate to="/signin"/>
     }
--- Real Estate Web Dev/src/hooks/useAuthStatus.jsx
+++ Real Estate Web Dev/src/hooks/useAuthStatus.jsx
@@ -1,13 +1,22 @@
-import { getAuth } from "firebase/auth";
+import { set } from "@cloudinary/url-gen/actions/variable";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 
 function useAuthStatus() {
   const [loggedIn, setLoggedIn] = useState(false);
-  const [checkingStatus, setCheckingStatus] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
 
-  useEffect
+  useEffect(()=>{
+   const auth = getAuth();
+   onAuthStateChanged(auth,(user)=>{
+    if(user){
+        setLoggedIn(true)
+    }
+    setCheckingStatus(false)
+   })
+  },[])
   
-  return <div></div>;
+  return {loggedIn,checkingStatus}
 }
 
 export default useAuthStatus;
