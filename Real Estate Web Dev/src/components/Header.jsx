57509f119a278d303389820b472ea6d9cca622eb
HarshanaPrabhath <harshanaprabhath147@gmail.com>
2025-03-11 20:21:03 +0530

    header component - make it dynamic

--- Real Estate Web Dev/src/components/Header.jsx
+++ Real Estate Web Dev/src/components/Header.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import React, { useEffect, useState } from 'react'
 import {useLocation, useNavigate } from 'react-router-dom'
 
 
 function Header() {
+    // to change sign in and profile image
+    const [pageState,setPageState] = useState("Sign in")
+
+    const auth = getAuth();
     const location = useLocation()
     const navigate = useNavigate()
+
+    useEffect(()=>{
+        onAuthStateChanged(auth,(user)=>{
+        if(user){
+          setPageState("Profile")
+        }else{
+          setPageState("Sign in")
+        }}
+        )
+    },[auth])
+    //path match to navigation bar border
     function pathMatchRoute(route){
         if (route === location.pathname ){
             return true; 
@@ -23,9 +39,10 @@ function Header() {
                 ${pathMatchRoute("/") && "border-b-white"} `}>Home</li>
             <li onClick={()=>navigate('/offers')}className={`text-lg text-white border-b-[3px] border-b-transparent py-3 cursor-pointer transition-transform duration-300 hover:scale-110
                 ${pathMatchRoute("/offers") && "border-b-white"} `}>Offers</li>
-            <li onClick={()=>navigate('/signin')} className={`text-lg text-white border-b-[3px] border-b-transparent py-3 cursor-pointer transition-transform duration-300 hover:scale-110
-                ${pathMatchRoute("/signin") && "border-b-white"} `}>Sign in</li>
+            <li onClick={()=>navigate('/profile')} className={`text-lg text-white border-b-[3px] border-b-transparent py-3 cursor-pointer transition-transform duration-300 hover:scale-110
+                ${(pathMatchRoute("/signin") || pathMatchRoute("/profile")) && "border-b-white"} `}>{pageState}</li>
            </ul>
+           {/* <img src={imgSrc}></img> */}
         </div>
       </header>
     </div>
